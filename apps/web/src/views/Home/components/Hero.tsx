ace80d9d204ced5e35e4e09b226946ed848adfae
caribdao <167460286+caribdao@users.noreply.github.com>
2024-04-24 16:17:03 -0500

    Update Hero.tsx

--- apps/web/src/views/Home/components/Hero.tsx
+++ apps/web/src/views/Home/components/Hero.tsx
@@ -231,7 +231,7 @@ const Hero = () => {
             </StyledText>
             {isMobile && <br />}
             <StyledText display="inline-block" lineHeight="110%" fontWeight={600} color="text">
-              {t('with Gas!🌊🚀')}
+              {t('with Gas!🌊')}
             </StyledText>
           </Text>
           <Text
