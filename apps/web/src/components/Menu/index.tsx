075f0c7ca3d138638316a58748d6232732665abb
caribdao <167460286+caribdao@users.noreply.github.com>
2024-05-08 17:03:00 -0500

    Update index.tsx
    fix BUY CARIB link

--- apps/web/src/components/Menu/index.tsx
+++ apps/web/src/components/Menu/index.tsx
@@ -126,7 +126,7 @@ const Menu = (props) => {
         activeItem={activeMenuItem?.href}
         activeSubItem={activeSubMenuItem?.href}
         buyCakeLabel={t('Buy CARIB')}
-        buyCakeLink="/swap?inputCurrency=0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82&outputCurrency=0x9f8b8FE01b26957cf3dcd6FBd3675053bA2c02C8"
+        buyCakeLink="/swap?outputCurrency=0x9f8b8FE01b26957cf3dcd6FBd3675053bA2c02C8"
         {...props}
       />
     </>
