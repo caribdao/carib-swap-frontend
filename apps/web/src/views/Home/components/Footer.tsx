a5518c35aa6c8318748b9192af431256f75b6f23
caribdao <167460286+caribdao@users.noreply.github.com>
2024-05-24 13:51:10 -0500

    Update Footer.tsx

--- apps/web/src/views/Home/components/Footer.tsx
+++ apps/web/src/views/Home/components/Footer.tsx
@@ -81,7 +81,7 @@ const ImageWrapper = styled.div`
 const ImageBox: React.FC = () => {
   return (
     <ImageWrapper>
-      <Image className="pancake" src={`${ASSET_CDN}/web/landing/cta-pancake.png`} alt="pancake" />
+      <Image className="pancake" src={`/images/caribdao/discord_icon.png`} alt="discord" />
       <Image className="rock" src={`${ASSET_CDN}/web/landing/cta-rock.png`} alt="rock" />
       <Image className="big-pancake" src={`${ASSET_CDN}/web/landing/cta-pancake-big.png`} alt="big-pancake" />
       <Image className="rock2" src={`${ASSET_CDN}/web/landing/cta-rock-2.png`} alt="rock2" />
