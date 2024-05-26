9921d343fa8fb3283d812ce3904d4dcbf7222c26
caribdao <167460286+caribdao@users.noreply.github.com>
2024-05-25 21:09:26 -0500

    Update LogoRound.tsx

--- packages/uikit/src/components/Svg/Icons/LogoRound.tsx
+++ packages/uikit/src/components/Svg/Icons/LogoRound.tsx
@@ -3,7 +3,7 @@ import Svg from "../Svg";
 import { SvgProps } from "../types";
 
 const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
-  return <img src="/images/caribswap/logo.png" height="20px" width="20px" alt="logo" />;
+  return <img src="/cakelogo.png" height="20px" width="20px" alt="logo" />;
 };
 
 export default Icon;
