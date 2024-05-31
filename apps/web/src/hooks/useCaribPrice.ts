0f5beac0c43d56bfc9ce143dd8e584d2b855393c
caribdao <167460286+caribdao@users.noreply.github.com>
2024-05-31 00:43:02 -0500

    Update useCaribPrice.ts

--- apps/web/src/hooks/useCaribPrice.ts
+++ apps/web/src/hooks/useCaribPrice.ts
@@ -7,26 +7,25 @@ import { publicClient } from 'utils/wagmi'
 import { formatUnits } from 'viem'
 import { FAST_INTERVAL } from 'config/constants'
 import { useQuery } from '@tanstack/react-query'
-import { getTokenPrice } from '../../../../packages/smart-router/evm/v3-router/utils/pool'
+// import { getTokenPrice } from '../../../../packages/smart-router/evm/v3-router/utils/pool'
 
 // for migration to bignumber.js to avoid breaking changes
 export const useCaribPrice = ({ enabled = true } = {}) => {
   const { data } = useQuery<BigNumber, Error>({
     queryKey: ['caribPrice'],
-    queryFn: async () => new BigNumber(await getTokenPrice('0xfA4Ac363C455fF9cc3348616a26f1eAE258AC665','0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c','0x9f8b8FE01b26957cf3dcd6FBd3675053bA2c02C8')),
+    queryFn: async () => new BigNumber(await getCaribPriceFromOracle()),
     staleTime: FAST_INTERVAL,
     refetchInterval: FAST_INTERVAL,
     enabled,
   })
   return data ?? BIG_ZERO
 }
-
-{/* export const getCaribPriceFromOracle = async () => {
+ export const getCaribPriceFromOracle = async () => {
   const data = await publicClient({ chainId: ChainId.BSC }).readContract({
     abi: chainlinkOracleABI,
     address: '0x9f8b8fe01b26957cf3dcd6fbd3675053ba2c02c8',
     functionName: 'latestAnswer',
-  }) */}
+  }) 
 
   return formatUnits(data, 8)
 }
