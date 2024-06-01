b735f9ac2dd30aab5697feb458dbff986d12ab8f
caribdao <167460286+caribdao@users.noreply.github.com>
2024-06-01 18:53:47 -0500

    Update index.tsx

--- apps/web/src/views/Home/index.tsx
+++ apps/web/src/views/Home/index.tsx
@@ -43,7 +43,7 @@ const Home: React.FC<React.PropsWithChildren> = () => {
           }
           [data-theme='dark'] #home-1 .page-bg {
             // background: radial-gradient(103.12% 50% at 50% 50%, #21193a 0%, #191326 100%);
-            background-image: url('/images/caribswap/backgrounds/temphome.jpg');
+            background-image: url('/images/caribswap/backgrounds/exchange.jpg');
             background-size: cover;
           }
           #home-2 .page-bg {
