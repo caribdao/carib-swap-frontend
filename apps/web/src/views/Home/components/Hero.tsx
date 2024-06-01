7b624aeb6c9f3afeb33a860e14797d7238540808
caribdao <167460286+caribdao@users.noreply.github.com>
2024-06-01 18:04:50 -0500

    Update Hero.tsx

--- apps/web/src/views/Home/components/Hero.tsx
+++ apps/web/src/views/Home/components/Hero.tsx
@@ -216,7 +216,16 @@ const Hero = () => {
         pl={['0px', '0px', '0px', '30px']}
         id="homepage-hero"
       >
-        <Flex flex="1" flexDirection="column" background= "rgba(0, 0, 0, 0.5)">
+        <Flex flex="1" flexDirection="column">
+           <style>
+              {`
+                [data-theme='dark'] {
+                  background: rgba(0, 0, 0, 0.5);
+                  padding: 20px;
+                  border-radius: 20px;
+                }
+              `}
+            </style>
           <Text textAlign={isMobile || isMd ? 'center' : 'left'} pr={isMobile ? 0 : '10px'} mb="16px">
             <StyledText display="inline-block" lineHeight="110%" fontWeight={600} color="text" mr="8px">
               {t("We")}
