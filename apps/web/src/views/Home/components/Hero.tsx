b5e3be6c7627a9826199349b1254b9b6ec7b98be
caribdao <167460286+caribdao@users.noreply.github.com>
2024-06-01 19:40:50 -0500

    Update Hero.tsx

--- apps/web/src/views/Home/components/Hero.tsx
+++ apps/web/src/views/Home/components/Hero.tsx
@@ -199,9 +199,6 @@ const Hero = () => {
           [data-theme='dark'] .slide-svg-light {
             display: none;
           }
-           [data-theme='dark'] .slogan {
-            display: none;
-          }
         `}
       </style>
       <BgWrapper>
@@ -219,7 +216,14 @@ const Hero = () => {
         pl={['0px', '0px', '0px', '30px']}
         id="homepage-hero"
       >
-        <Flex id="slogan" flex="1" flexDirection="column" background="rgba(0, 0, 0, 0.2)" padding="20px" borderRadius="20px">
+        <Flex 
+          id="slogan"
+          flex="1" 
+          flexDirection="column" 
+          background= {theme.isDark ? 'rgba(0, 0, 0, 0.2)' : ''}
+          padding="20px"
+          borderRadius="20px"
+          >
           <Text textAlign={isMobile || isMd ? 'center' : 'left'} pr={isMobile ? 0 : '10px'} mb="16px">
             <StyledText display="inline-block" lineHeight="110%" fontWeight={600} color="text" mr="8px">
               {t("We")}
