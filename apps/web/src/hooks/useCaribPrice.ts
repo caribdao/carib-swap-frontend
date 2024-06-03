38cfd478ed60933c22ca024e4b074ab2c649e6c4
caribdao <167460286+caribdao@users.noreply.github.com>
2024-06-03 04:45:05 -0500

    Update useCaribPrice.ts

--- apps/web/src/hooks/useCaribPrice.ts
+++ apps/web/src/hooks/useCaribPrice.ts
@@ -1,25 +1,26 @@
 import { ChainId } from '@pancakeswap/chains'
 import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
-// import { chainlinkOracleCAKE } from '@pancakeswap/prediction'
+import { chainlinkOracleCAKE } from '@pancakeswap/prediction'
 import BigNumber from 'bignumber.js'
 import { chainlinkOracleABI } from 'config/abi/chainlinkOracle'
 import { publicClient } from 'utils/wagmi'
 import { formatUnits } from 'viem'
 import { FAST_INTERVAL } from 'config/constants'
 import { useQuery } from '@tanstack/react-query'
-// import { getTokenPrice } from '../../../../packages/smart-router/evm/v3-router/utils/pool'
+import { CARIB_PRICE_DEXSCREENER_API_URL } from 'config/constants/endpoints'
 
 // for migration to bignumber.js to avoid breaking changes
 export const useCaribPrice = ({ enabled = true } = {}) => {
   const { data } = useQuery<BigNumber, Error>({
     queryKey: ['caribPrice'],
-    queryFn: async () => new BigNumber(await getCaribPriceFromOracle()),
+    queryFn: async () => new BigNumber(await getCaribPriceDs()),
     staleTime: FAST_INTERVAL,
     refetchInterval: FAST_INTERVAL,
     enabled,
   })
   return data ?? BIG_ZERO
 }
+
  export const getCaribPriceFromOracle = async () => {
   const data = await publicClient({ chainId: ChainId.BSC }).readContract({
     abi: chainlinkOracleABI,
@@ -29,3 +30,25 @@ export const useCaribPrice = ({ enabled = true } = {}) => {
 
   return formatUnits(data, 8)
 }
+
+
+// Define the function to fetch and return the priceUsd as a BigNumber
+export const getCaribPriceDs = async () => {
+  try {
+    const response = await fetch(CARIB_PRICE_DEXSCREENER_API_URL);
+    if (!response.ok) {
+      return 'NaN';
+    }
+    const data = await response.json();
+
+    // Extract the priceUsd from the API response
+    const priceUsd = data?.pair?.priceUsd;
+
+    // Convert priceUsd to a BigNumber and return it
+    return priceUsd;
+  } catch (error) {
+    console.error('Failed to fetch priceUsd:', error);
+
+    return '0'; // Return a BigNumber with value 0 in case of error
+  }
+};
