4a47366b20a49532a0a87710dab9b37dd32d5be6
caribdao <167460286+caribdao@users.noreply.github.com>
2024-06-03 05:43:56 -0500

    Update CaribPrice.tsx

--- packages/uikit/src/components/CaribPrice/CaribPrice.tsx
+++ packages/uikit/src/components/CaribPrice/CaribPrice.tsx
@@ -37,7 +37,7 @@ const CaribPrice: React.FC<React.PropsWithChildren<Props>> = ({
       target="_blank"
     >
       <LogoRound width="24px" mr="8px" />
-      <Text color={color} bold>{` $${caribPriceUsd}`}</Text>
+      <Text color={color} bold>{`\xa0 $${caribPriceUsd}`}</Text>
     </PriceLink> 
   ) : showSkeleton ? (
      <Skeleton width={80} height={24} />  
